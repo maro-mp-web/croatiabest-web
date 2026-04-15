455ea4e3fd69b352d8d9bae6ffebf9c4556f3a6b
Maro Pincevic <maro.webdeveloper@gmail.com>
2026-04-15 14:36:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3765038369-e65c9",
+  "appId": "1:872862292418:web:89d9f8e0b8345e50a01e5b",
+  "apiKey": "AIzaSyAXQ31rMXcSlVXTHxsRLebSDN7FxsDDytI",
+  "authDomain": "studio-3765038369-e65c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "872862292418"
+};
