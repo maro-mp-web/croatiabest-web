7ef5ff01dbef053c948c5b0334abd9f78b496e26
maro-mp-web <maro.webdeveloper@gmail.com>
2026-06-27 16:42:28 +0200

    Allow maps.googleapis.com remote pattern under images config in next.config.ts to permit Google Street View static imagery rendering

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
